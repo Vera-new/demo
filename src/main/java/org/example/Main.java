81b119de14e699a6127c36289fb948bf50166f7c
Vera-new <o.vorona6@yandex.ru>
2024-08-31 20:55:32 +0700

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Первоначальная версия");
+        System.out.print("Первое изменениепше");
+    }
+}
